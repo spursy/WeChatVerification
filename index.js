a1dd0aa5321ec6613ce5faf1e140b451b14812e6
spursy <582214816@qq.com>
2017-11-21 13:28:51 +0800

    add server favicon and load static resource

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const Koa = require('koa');
 const app = new Koa();
 const config = require('./server/config');
 const log = require('./common/log');
+const favicon = require('koa-favicon');
+const koa_static = require('koa-static-server');
+process.title = '微信验证';
 
 // response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+// app.use(ctx => {
+//   ctx.body = 'Hello Koa';
+// });
+app.use(koa_static({rootDir: __dirname + '/web'}));
+
+app.use(favicon(__dirname + '/web/image/me.png'));
 
 app.listen(config.PORT);
 var TAG = log.TAG();
