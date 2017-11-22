114ad09666fc6bbd149c9e4a76a5c10beef8fedf
spursy <582214816@qq.com>
2017-11-22 15:25:53 +0800

    add jquery and bootstrap reference

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const app = new Koa();
 const config = require('./server/config');
 const log = require('./common/log');
 const favicon = require('koa-favicon');
-const koa_static = require('koa-static-server');
+// const koa_static = require('koa-static-server');
+const koa_static = require('koa-static');
+
 
 process.title = '微信验证';
 
-app.use(koa_static({rootDir: __dirname + '/web', rootPath: '/admin'}));
-app.use(favicon(__dirname + '/web/image/me.png'));
-
-// router
-require('./server/router.js')(app);
+//add static file
+app.use(koa_static(__dirname+'/node_modules'));
+app.use(koa_static(__dirname+'/web'));
+app.use(favicon('/image/me.png'));
 
 app.listen(config.PORT);
 var TAG = log.TAG();
